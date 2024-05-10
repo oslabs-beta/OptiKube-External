7e5e8cb0ee247c70c88a88e0b5bb1ce86b483eac
George Huang <gzh@alumni.princeton.edu>
2024-05-09 19:41:27 -0700

    boiler plate react app with basic landing page

---
+++ client/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div className="hero">
+          <div className="content">
+            <h1>Optimize Kubernetes</h1>
+            <p>Enhance your Kubernetes experience</p>
+            <button className="button primary">Quick Start</button>
+            <button className="button secondary">GitHub</button>
+          </div>
+        </div>
+    )
+}
+
+export default Hero;
